f30d03f049305dc890ca12bbe8ecd71741cbf9e5
Jason <Jpihlk@yahoo.com>
2020-04-09 21:33:16 -0400

    Set for tutorial

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { useFormik } from 'formik';
+
+const SignupForm = () => {
+  // Pass the useFormik() hook initial form values and a submit function that will
+  // be called when the form is submitted
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="email">Email Address</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};  
+  // ========================================
+  
+  ReactDOM.render(
+    <SignupForm />,
+    document.getElementById('root')
+  );
+  
