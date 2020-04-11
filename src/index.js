b6f317b5f3e731cd15c0cd4edcfeda5c90924a39
Jason <Jpihlk@yahoo.com>
2020-04-11 17:08:26 -0400

    multi inputs

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const SignupForm = () => {
   const formik = useFormik({
     initialValues: {
       email: '',
+      firstName: '',
+      lastName: '',
     },
     onSubmit: values => {
       alert(JSON.stringify(values, null, 2));
@@ -15,6 +17,24 @@ const SignupForm = () => {
   });
   return (
     <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="firstName">First Name:</label>
+      <input
+        id="firstName"
+        name="firstName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.firstName}
+      />
+      
+      <label htmlFor="">Last Name:</label>
+      <input
+        id="lastName"
+        name="lastName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.lastName}
+      />
+
       <label htmlFor="email">Email Address</label>
       <input
         id="email"
@@ -23,6 +43,7 @@ const SignupForm = () => {
         onChange={formik.handleChange}
         value={formik.values.email}
       />
+
       <button type="submit">Submit</button>
     </form>
   );
