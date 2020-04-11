e3358dd7d035cd7ada2ccbbe654350b039cdf2fb
Jason <Jpihlk@yahoo.com>
2020-04-11 18:19:21 -0400

    validating whole form on blur, keystroke and pre submit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { useFormik } from 'formik';
 
+
+// A custom validation function. This must return an object
+// which keys are symmetrical to our values/initialValues
+
+const validate = values => {
+    const errors = {};
+    if (!values.firstName) {
+      errors.firstName = 'Required';
+    } else if (values.firstName.length > 15) {
+      errors.firstName = 'Must be 15 characters or less';
+    }
+  
+    if (!values.lastName) {
+      errors.lastName = 'Required';
+    } else if (values.lastName.length > 20) {
+      errors.lastName = 'Must be 20 characters or less';
+    }
+  
+    if (!values.email) {
+      errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      errors.email = 'Invalid email address';
+    }
+    return errors;
+}  
+
+
+
+
+
 const SignupForm = () => {
   // Pass the useFormik() hook initial form values and a submit function that will
   // be called when the form is submitted
@@ -11,6 +41,7 @@ const SignupForm = () => {
       firstName: '',
       lastName: '',
     },
+    validate,
     onSubmit: values => {
       alert(JSON.stringify(values, null, 2));
     },
@@ -25,7 +56,7 @@ const SignupForm = () => {
         onChange={formik.handleChange}
         value={formik.values.firstName}
       />
-      
+      {formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
       <label htmlFor="">Last Name:</label>
       <input
         id="lastName"
@@ -34,7 +65,7 @@ const SignupForm = () => {
         onChange={formik.handleChange}
         value={formik.values.lastName}
       />
-
+      {formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
       <label htmlFor="email">Email Address</label>
       <input
         id="email"
@@ -43,7 +74,7 @@ const SignupForm = () => {
         onChange={formik.handleChange}
         value={formik.values.email}
       />
-
+      {formik.errors.email ? <div>{formik.errors.email}</div> : null}
       <button type="submit">Submit</button>
     </form>
   );
